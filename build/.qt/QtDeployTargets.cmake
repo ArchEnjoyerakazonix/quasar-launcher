8476a421f2893501f0793b986d7bb3d85ae715bf
ArchEnjoyerakazonix <archenjoyerakazonix@users.noreply.github.com>
2026-08-08 22:34:17 +0400

    Add gitignore and remove build artifacts

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_nexus-launcher_FILE /home/archuser/Projects/nexus-launcher/build/nexus-launcher)
-set(__QT_DEPLOY_TARGET_nexus-launcher_TYPE EXECUTABLE)
--- build/.qt/nexus-launcher_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "nexus-launcher")
-set(working_dir "/home/archuser/Projects/nexus-launcher")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/home/archuser/Projects/nexus-launcher/build/.qt/nexus-launcher_res.txt")
